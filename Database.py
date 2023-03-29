58a40e664185935ff3a31f4f4e4816f54097f880
Roman Kornoukhov <kornoukhov.omsk@yandex.ru>
2023-03-29 16:37:38 +0600

    Изменён Database.py

--- Database.py
+++ Database.py
@@ -1,14 +1,12 @@
 import logging
 import sqlite3
 
-sqlite_connection = sqlite3.connect('data.db')  # подключение к БД SQLite
-cur = sqlite_connection.cursor()  # задаём указатель БД
-
 
 def getGroups() -> list:
     try:
-        res = cur.execute("SELECT DISTINCT groupname FROM students")
-        Groups = [i[0] for i in res.fetchall()]  # res.fetchall() возвращает кортеж, оттуда нужны только чистые строки
+        sqlite_connection = sqlite3.connect('data.db')  # подключение к БД SQLite
+        cur = sqlite_connection.cursor()  # задаём указатель БД
+        Groups = [i[0] for i in cur.fetchall()]  # cur.fetchall() возвращает кортеж, оттуда нужны только чистые строки
         logging.info("Список групп успешно получен из БД")
         return Groups
     except:
