4e3d91932e4d3dafab855ed63bbd425c943e87d4
Roman Kornoukhov <kornoukhov.omsk@yandex.ru>
2023-03-29 21:38:03 +0600

    Возможность запроса о успеваемости/посещаемости выбранного студента

--- Database.py
+++ Database.py
@@ -21,8 +21,40 @@ def getStudents(group: str) -> list:
         cur = sqlite_connection.cursor()  # задаём указатель БД
         cur = cur.execute("SELECT fullname FROM students WHERE groupname = '{}';".format(group))
         Students = [i[0] for i in cur.fetchall()]  # cur.fetchall() возвращает кортеж, оттуда нужны только чистые строки
-        logging.info("Список студентов группы {} успешно получен из БД".format(group))
+        logging.info("Список студентов группы '{}' успешно получен из БД".format(group))
         return Students
     except:
-        print("ОШИБКА! Не удалось получить студентов группы {}!".format(group))
+        print("ОШИБКА! Не удалось получить студентов группы '{}'!".format(group))
+        exit(2)
+
+
+def getRating(student: str) -> dict:
+    try:
+        sqlite_connection = sqlite3.connect('data.db')  # подключение к БД SQLite
+        cur = sqlite_connection.cursor()  # задаём указатель БД
+
+        # получим оценки студента, чтобы найти среднюю оценку (успеваемость)
+        cur = cur.execute("SELECT * FROM rating WHERE fullname = '{}';".format(student))
+        marks_tuple = cur.fetchall()  # получаем всю строку про выбранного студента
+
+        rating = 0  # сумматор для рейтинга (кол-во баллов)
+        skipped = 0  # счётчик пропущенных занятий
+        lesson_count = len(marks_tuple[0]) - 1  # кол-во занятий = кол-во столбцов без учёта имени
+
+        # Перебираем элементы, полученным из ДБ, кроме первой колонки (т.к. это имя студента)
+        for i in range(lesson_count):
+            element = marks_tuple[0][i + 1]
+            # добавляем существующие элементы (тип не NULL (None))
+            if element:
+                rating += element
+            else:
+                skipped += 1
+
+        attendance = (lesson_count - skipped) / lesson_count  # посещаемость (дробью)
+        study_info = {'rating': rating, 'attendance': attendance, 'lesson_count': lesson_count,
+                      'attended': (lesson_count - skipped)}
+        logging.info("Список студентов группы '{}' успешно получен из БД".format(student))
+        return study_info
+    except:
+        print("ОШИБКА! Не удалось получить информацию о студенте '{}'!".format(student))
         exit(2)
